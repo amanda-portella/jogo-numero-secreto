6e051ded694a9942c9ebbc3a06984643ea963fca
unknown <amandaportella.m@gmail.com>
2022-11-07 17:45:17 -0300

    Adicionado reconhecimento de voz para a palabra Game Over

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,18 @@ function verificaSeOChutePossuiValorValido(chute) {
     const numero = +chute;
 
     if(chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor Inválido</div>'
-        return;
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =`
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "#80489C";
+        } else {
+            elementoChute.innerHTML += '<div>Valor Inválido</div>'
+            return;
+        }
     }
 
     if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
